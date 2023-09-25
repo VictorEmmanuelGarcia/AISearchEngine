2f2454edaa5a12c95bdd0c46aad743c74c5776ed
Timothy Janssen Taboada <timothyjanssen.taboada@cit.edu>
2023-09-25 14:35:24 +0800

    Add routing

---
+++ Frontend/ipams-search/src/screens/search/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Search.css"
+
+const Search = () => {
+    return (
+        <div>
+            search page
+        </div>
+    );
+};
+
+export default Search;
