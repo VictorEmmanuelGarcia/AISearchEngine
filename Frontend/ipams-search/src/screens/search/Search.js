d014c2488be8b16ef458985878d3874c6a1d1646
Timothy Janssen Taboada <timothyjanssen.taboada@cit.edu>
2023-09-25 16:14:50 +0800

    Initial Search page design

--- Frontend/ipams-search/src/screens/search/Search.js
+++ Frontend/ipams-search/src/screens/search/Search.js
@@ -5,7 +5,21 @@ import "./Search.css"
 const Search = () => {
     return (
         <div>
-            search page
+            <image src="" alt="insert image here"/>
+            <div className="title">
+                <h1>
+                    Narrative Association for Linked Content (NALC) 
+                </h1>
+                <h4>
+                    Your helpful search buddy for IPAMS.
+                </h4>
+            </div>
+            <div className="input-container">
+                <input
+                    type="text"
+                    id="query"
+                />
+            </div>
         </div>
     );
 };
