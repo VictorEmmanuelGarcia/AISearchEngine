1afe215def4692e5499b7485bab1d161e5e47c86
John William Miones <johnwilliam.miones@cit.edu>
2023-09-27 21:03:09 +0800

    Add model

--- Backend/IpamsSearch/search/models.py
+++ Backend/IpamsSearch/search/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class researchpaper(models.Model):
+    TITLE_CHOICES = (
+        (1, 'Proposal'),
+        (2, 'Thesis/Research'),
+        (3, 'Project'),
+    )
+
+    CLASSIFICATION_CHOICES = (
+        (1, 'Basic Research'),
+        (2, 'Applied Research'),
+    )
+
+    title = models.CharField(max_length=255)
+    abstract = models.TextField()
+    year = models.IntegerField()
+    record_type = models.IntegerField(choices=TITLE_CHOICES)
+    classification = models.IntegerField(choices=CLASSIFICATION_CHOICES)
+    psc_ed = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
