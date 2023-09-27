9b441ab18fd78ad159454357dd20a85808d618b7
Timothy Janssen Taboada <timothyjanssen.taboada@cit.edu>
2023-09-27 22:42:29 +0800

    Add chat response & conditional rendering

--- Frontend/ipams-search/src/screens/search/Search.js
+++ Frontend/ipams-search/src/screens/search/Search.js
@@ -1,24 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Search.css"
 
 const Search = () => {
+    const [query, setQuery] = useState('');
+    const [response, setResponse] = useState([]);
+    const [chatHistory, setHistory] = useState([]);
+
+    
+    const handleSearch = () => {
+        // Perform a query here (replace with your actual query logic)
+        const sampleResponse = [
+            { id: 1, name: 'Result 1' },
+            { id: 2, name: 'Result 2' },
+            { id: 3, name: 'Result 3' },
+        ];
+
+        // Update chat history with user query and bot response
+        const newChatHistory = [
+            ...chatHistory,
+            { id: 'user', message: query },
+            { id: 'bot', message: sampleResponse },
+        ];
+
+        setResponse(sampleResponse);
+        setHistory(newChatHistory);
+        setQuery('');
+    };
+
+    const chatMessages = [
+        // User query
+        {
+            id: 'user',
+            message: query,
+        },
+        // Bot response
+        {
+            id: 'bot',
+            message: (
+            <div>
+                {/* Bot's response message */}
+                <p>Here are the search results:</p>
+                {/* Display the tabular response */}
+                <table>
+                <thead>
+                    <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {response.map((item) => (
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.name}</td>
+                    </tr>
+                    ))}
+                </tbody>
+                </table>
+            </div>
+            ),
+        },
+    ];
+
     return (
         <div>
-            <image src="" alt="insert image here"/>
-            <div className="title">
-                <h1>
-                    Narrative Association for Linked Content (NALC) 
-                </h1>
-                <h4>
-                    Your helpful search buddy for IPAMS.
-                </h4>
+            {chatHistory.length === 0 && (
+                <div className="title-logo-container">
+                <image src="" alt="insert image here"/>
+                <h1>Narrative Association for Linked Content (NALC)</h1>
+                <h4>Your helpful search buddy for IPAMS.</h4>
+                </div>
+            )}
+
+            {/* Chat container with chat history */}
+            <div className="chat-container">
+                {chatHistory.map((message, index) => (
+                <div key={index} className={`chat-message ${message.id}`}>
+                    {message.id === 'bot' ? (
+                    <div>
+                        {/* Bot's response message */}
+                        <p>Here are the search results:</p>
+                        {/* Display the tabular response */}
+                        <table>
+                        <thead>
+                            <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {message.message.map((item) => (
+                            <tr key={item.id}>
+                                <td>{item.id}</td>
+                                <td>{item.name}</td>
+                            </tr>
+                            ))}
+                        </tbody>
+                        </table>
+                    </div>
+                    ) : (
+                    message.message
+                    )}
+                </div>
+                ))}
             </div>
-            <div className="input-container">
+
+            <div className="user-input-container">
                 <input
-                    type="text"
-                    id="query"
+                type="text"
+                id="query"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
                 />
+                <button onClick={handleSearch}>Send</button>
             </div>
         </div>
     );
