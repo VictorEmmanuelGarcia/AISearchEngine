09b89bfc6cdf1751e4db6703c82b2ea2fd4c2871
Andre Lennard S. Uy <andrelennard.uy@cit.edu>
2023-10-02 16:57:23 +0800

    made a template for BE DB

--- Backend/IpamsSearch/IpamsSearch/settings.py
+++ Backend/IpamsSearch/IpamsSearch/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'IpamsSearch.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'research_data',      # Replace with your database name
-        'USER': 'root',     # Replace with your MySQL username
-        'PASSWORD': '', # Replace with your MySQL password
+        'NAME': 'your_database_name',      # Replace with your database name
+        'USER': 'your_mysql_username',     # Replace with your MySQL username
+        'PASSWORD': 'your_mysql_password', # Replace with your MySQL password
         'HOST': '127.0.0.1',              # Set the host to your MySQL server (localhost)
         'PORT': '3306',                   # MySQL default port
     }
