6adac1fcb45e6e5f1063eadd35803e339d915c80
Timothy Janssen Taboada <timothyjanssen.taboada@cit.edu>
2023-10-03 22:51:52 +0800

    Displays tabular results

--- Frontend/ipams-search/src/screens/search/Search.js
+++ Frontend/ipams-search/src/screens/search/Search.js
@@ -10,27 +10,52 @@ const Search = () => {
     const [response, setResponse] = useState([]);
     const [chatHistory, setHistory] = useState([]);
 
-    
+    const categoryMappings = {
+        "14": "Education Science and Teacher Training",
+        "18": "Fine and Applied Arts",
+        "22": "Humanities",
+        "26": "Religion and Theology",
+        "30": "Social and Behavioral Sciences",
+        "34": "Business Administration and Related",
+        "38": "Law and Jurisprudence",
+        "42": "Natural Science",
+        "46": "Mathematics",
+        "47": "IT-Related Disciplines",
+        "50": "Medical and Allied",
+        "52": "Trade, Craft and Industrial",
+        "54": "Engineering and Tech",
+        "58": "Architecture and Town Planning",
+        "62": "Agriculture, Forestry, Fisheries",
+        "66": "Home Economics",
+        "78": "Service Trades",
+        "84": "Mass Communication and Documentation",
+        "89": "Other Disciplines",
+        "90": "Maritime",
+        "00": "General",
+    }
+
     const handleSearch = () => {
         // Perform a query here (replace with your actual query logic)
         axios.get('http://127.0.0.1:8000/search/?query=${query}')
-            .then(response => {
-                setResponse(response.data);
+            .then(apiResponse => {
+                const newChatHistory = [
+                    ...chatHistory,
+                    { id: 'user', message: query },
+                    { id: 'bot', message: apiResponse.data },
+                ];
+
+                const updatedResponseData = apiResponse.data.map(item => ({
+                    ...item,
+                    psc_ed: categoryMappings[item.psc_ed] || item.psc_ed, // Replace if found in mappings, else keep the original value
+                }));
+                  
+                setResponse(updatedResponseData);
+                setHistory(newChatHistory);
+                setQuery('');
             })
             .catch(error => {
                 console.error(error);
         });
-
-        //Update chat history with user query and bot response
-        // const newChatHistory = [
-        //     ...chatHistory,
-        //     { id: 'user', message: query },
-        //     { id: 'bot', message: sampleResponse },
-        // ];
-
-        // setResponse(sampleResponse);
-        // setHistory(newChatHistory);
-        setQuery('');
     };
 
      // Function to handle input changes and update the state
@@ -38,45 +63,6 @@ const Search = () => {
         setQuery(event.target.value);
     };
 
-    const chatMessages = [
-        // User query
-        {
-            id: 'user',
-            message: query,
-        },
-        // Bot response
-        {
-            id: 'bot',
-            message: (
-            <div>
-                {/* Bot's response message */}
-                <p>Here are the search results:</p>
-                {/* Display the tabular response */}
-                <table>
-                <thead>
-                    <tr>
-                    <th>Title</th>
-                    <th>Abstract</th>
-                    <th>Year</th>
-                    <th>Record Type</th>
-                    <th>Authors</th>
-                    <th>PSCED</th>
-                    <th>Classification</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {response.map((item) => (
-                    <tr key={item.id}>
-                        <td>{item.id}</td>
-                        <td>{item.name}</td>
-                    </tr>
-                    ))}
-                </tbody>
-                </table>
-            </div>
-            ),
-        },
-    ];
 
     return (
         <div className="container">
@@ -84,7 +70,7 @@ const Search = () => {
                 <div className="title-logo-container" id="cont">
                     <image src="" alt="insert image here"/>
                     <h1 className="textCenter">Narrative Association for Linked Content (NALC)</h1>
-                    <h4 className="textCenter">Your helpful search buddy for IPAMS.</h4>
+                    <h4 className="textCenter">Stand on the shoulders of giants.</h4>
                 </div>
             )}
             <br/>
@@ -109,17 +95,16 @@ const Search = () => {
                                 <th>Title</th>
                                 <th>Abstract</th>
                                 <th>Year</th>
-                                <th>Record Type</th>
-                                <th>Authors</th>
                                 <th>PSCED</th>
-                                <th>Classification</th>
                             </tr>
                         </thead>
                         <tbody>
                             {message.message.map((item) => (
                             <tr key={item.id}>
-                                <td>{item.id}</td>
-                                <td>{item.name}</td>
+                                <td>{item.title}</td>
+                                <td>{item.abstract}</td>
+                                <td>{item.year}</td>
+                                <td>{item.psc_ed}</td>
                             </tr>
                             ))}
                         </tbody>
