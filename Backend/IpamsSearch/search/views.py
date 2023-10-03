49bbab9135f6290d0bcc9979adce6ddcab890b62
John William Miones <johnwilliam.miones@cit.edu>
2023-10-03 12:43:29 +0800

    Add necessary views

--- Backend/IpamsSearch/search/views.py
+++ Backend/IpamsSearch/search/views.py
@@ -1,37 +1,22 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import SearchQuerySerializer, SearchResultSerializer
 from txtai.embeddings import Embeddings
-from .models import researchpaper
-from rest_framework import generics
-from .serializers import ResearchPaperSerializer
+from bookmark_function import create_bookmark
+from .models import Bookmark, BookmarkRP, researchpaper
+from rest_framework import generics, viewsets, status, permissions
+from .serializers import BookmarkRPSerializer, BookmarkSerializer, ResearchPaperSerializer
 from semantic_search import perform_semantic_search
 from django.http import JsonResponse
+from rest_framework.decorators import action
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from django.shortcuts import get_object_or_404
 
+# Semantic Search Views
 embeddings = Embeddings({
     "path": "sentence-transformers/all-MiniLM-L6-v2"
 })
-
-class SemanticSearchView(APIView):
-    def post(self, request):
-        serializer = SearchQuerySerializer(data=request.data)
-        if serializer.is_valid():
-            query = serializer.validated_data['query']
-
-            # Perform semantic search using txtai library
-            results = embeddings.search(query, 5)  # Adjust the number of results as needed
-
-            search_results = []
-            for index, similarity in results:
-                paper = researchpaper.objects.get(id=index + 1)  # Assuming your model has auto-incremented IDs
-                serializer = SearchResultSerializer(paper, context={'similarity': similarity})
-                search_results.append(serializer.data)
-
-            return Response(search_results)
-        else:
-            return Response(serializer.errors, status=400)
-
 
 class ResearchPaperListView(generics.ListAPIView):
     queryset = researchpaper.objects.all()
@@ -46,3 +31,21 @@ class SearchView(APIView):
 
         serializer = ResearchPaperSerializer(search_results, many=True)
         return Response(serializer.data)
+
+# Bookmark Views
+# Create Bookmark View
+class CreateBookmarkView(generics.CreateAPIView):
+    queryset = Bookmark.objects.prefetch_related('research_papers')
+    serializer_class = BookmarkSerializer
+
+# Get Bookmarks View
+class ListBookmarksView(generics.ListAPIView):
+    serializer_class = BookmarkSerializer
+
+    def get_queryset(self):
+        # Retrieve the user ID from the URL parameter (e.g., /list-bookmarks/<user_id>/)
+        user_id = self.kwargs['user_id']
+
+        # Query the database to get all bookmarks for the specific user
+        queryset = Bookmark.objects.filter(user_id=user_id)
+        return queryset
