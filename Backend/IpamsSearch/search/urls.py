ac3bdf0753ecfdbdaf4176cbe140e5f5eeb74db8
John William Miones <johnwilliam.miones@cit.edu>
2023-10-03 12:43:43 +0800

    Add urls

--- Backend/IpamsSearch/search/urls.py
+++ Backend/IpamsSearch/search/urls.py
@@ -1,11 +1,13 @@
-# urls.py in your app
-from django.urls import path
-from .views import SearchView, SemanticSearchView
-from .views import ResearchPaperListView
-from . import views
+from django.urls import path, include
+from .views import SearchView, ResearchPaperListView, CreateBookmarkView, ListBookmarksView
+
 
 urlpatterns = [
-    # path('semantic-search/', SemanticSearchView.as_view(), name='semantic-search'),
-    # path('research-papers/', ResearchPaperListView.as_view(), name='research-papers'),
+    # Semantic Search URLs
+    path('research-papers/', ResearchPaperListView.as_view(), name='research-papers'),
     path('search/', SearchView.as_view(), name='search'),
+
+    # Bookmark URLs
+    path('create-bookmark/', CreateBookmarkView.as_view(), name='create-bookmark'),
+    path('list-bookmarks/<int:user_id>/', ListBookmarksView.as_view(), name='list-bookmarks'),
 ]
