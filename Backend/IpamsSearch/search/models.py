d90f48bf0016ab1814923303603122211d7819bd
John William Miones <johnwilliam.miones@cit.edu>
2023-10-03 12:41:54 +0800

    Add bookmarks

--- Backend/IpamsSearch/search/models.py
+++ Backend/IpamsSearch/search/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User # Django's built-in User model
 
 class researchpaper(models.Model):
     TITLE_CHOICES = (
@@ -22,3 +23,20 @@ class researchpaper(models.Model):
 
     def __str__(self):
         return self.title
+    
+    # Bookmark Model
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)  # Assuming you're using Django's User model
+    name = models.CharField(max_length=255)
+    research_papers = models.ManyToManyField(researchpaper, through='BookmarkRP')
+
+    def __str__(self):
+        return self.name
+
+# BookmarkRP Model (Join Table for Many-to-Many Relationship)
+class BookmarkRP(models.Model):
+    bookmark = models.ForeignKey(Bookmark, on_delete=models.CASCADE)
+    research_paper = models.ForeignKey(researchpaper, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.bookmark.name} - {self.research_paper.title}"
