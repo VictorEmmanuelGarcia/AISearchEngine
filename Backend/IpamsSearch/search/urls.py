b919bb4203cadf73cd82c0ed4410bdfa68212055
John William Miones <johnwilliam.miones@cit.edu>
2023-10-05 12:57:50 +0800

    Add urls for getResearchPaperbyId

--- Backend/IpamsSearch/search/urls.py
+++ Backend/IpamsSearch/search/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
-from .views import SearchView, ResearchPaperListView, CreateBookmarkView, ListBookmarksView
+from .views import SearchView, ResearchPaperListView, CreateBookmarkView, ListBookmarksView, GetResearchPaperById
 
 
 urlpatterns = [
     # Semantic Search URLs
-    path('research-papers/', ResearchPaperListView.as_view(), name='research-papers'),
     path('search/', SearchView.as_view(), name='search'),
+
+    # Research Paper URLs
+    path('research-papers/<int:pk>/', GetResearchPaperById.as_view(), name='get_research_paper_by_id'),
+    path('research-papers/', ResearchPaperListView.as_view(), name='research-papers'),
 
     # Bookmark URLs
     path('create-bookmark/', CreateBookmarkView.as_view(), name='create-bookmark'),
