5c1647a6e3027aad286729ad91391969fb38f6c5
John William Miones <johnwilliam.miones@cit.edu>
2023-10-08 14:42:56 +0800

    Delete unuse imports

--- Frontend/ipams-search/src/App.js
+++ Frontend/ipams-search/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./screens/login/login.js";
 import Search from "./screens/search/Search";
 import Home from "./screens/home/home";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
