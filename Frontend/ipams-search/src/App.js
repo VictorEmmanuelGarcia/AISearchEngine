48b0971b39688878617f153151f83183b473cb59
Andre Lennard S. Uy <andrelennard.uy@cit.edu>
2023-10-10 21:28:31 +0800

    Add Navbar to the application

--- Frontend/ipams-search/src/App.js
+++ Frontend/ipams-search/src/App.js
@@ -1,18 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./screens/login/Login";
 import Search from "./screens/search/Search";
+import Navbar from './Components/Navbar';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' exact element={<Login/>}/>
-      <Route path='/login' exact element={<Login/>}/>
-      <Route path='/search' exact element={<Search/>}/>
-    </Routes>
+      <div className='container-fluid'>
+        <Navbar/>
+        <Routes>
+          <Route path='/' exact element={<Login />} />
+          <Route path='/login' exact element={<Login />} />
+          <Route path='/search' exact element={<Search />} />
+        </Routes>
+      </div>
   );
 }
 
